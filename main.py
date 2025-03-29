3669408e6778b080fb38ea19ad4e9eac98d7a13a
nileshphapale142 <nileshphapale142@gmail.com>
2025-03-30 03:14:32 +0530

    Pipeline properties added

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from importlib.resources.readers import remove_duplicates
+
 import streamlit as st
 import requests
 import time
+import json
+import base64
+
+
+
 
 AUTH_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkNOdjBPSTNSd3FsSEZFVm5hb01Bc2hDSDJYRSIsImtpZCI6IkNOdjBPSTNSd3FsSEZFVm5hb01Bc2hDSDJYRSJ9.eyJhdWQiOiJodHRwczovL2FwaS5mYWJyaWMubWljcm9zb2Z0LmNvbSIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzRhYzUwMTA1LTBjNjYtNDA0ZS1hMTA3LTdjYmQ4YTlhNjQ0Mi8iLCJpYXQiOjE3NDMyNzk0MDAsIm5iZiI6MTc0MzI3OTQwMCwiZXhwIjoxNzQzMjg1MDg2LCJhY2N0IjowLCJhY3IiOiIxIiwiYWlvIjoiQVhRQWkvOFpBQUFBRFJDTFdWbFYydExnV3lncUJzd0JkbXRMQWNkUnpoMFlQODBVcVFnNjJwMy9PYk5VTUU3TXNHNG02OUVnVm41U3FYNjZlL0lnbXp5eWZlRnRkZVhkaytDNGtzbkhIdFduNDRDcnp1ZGVvWUl2djVVY2tqdERJS0JBcytiZjhVOW1HYndFNWkwbjVlN2hpWFVNazZDTG9nPT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiMThmYmNhMTYtMjIyNC00NWY2LTg1YjAtZjdiZjJiMzliM2YzIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJEZWJyb3kiLCJnaXZlbl9uYW1lIjoiQW5raXQiLCJpZHR5cCI6InVzZXIiLCJpcGFkZHIiOiIxNC45Ny4xNjQuNDYiLCJuYW1lIjoiQW5raXQgRGVicm95Iiwib2lkIjoiNGI4ZWJmYjYtODUxNC00YjBlLWEwODMtMDY4MjIzZGQwZWU0IiwicHVpZCI6IjEwMDMyMDA0MzhDODg1NDIiLCJyaCI6IjEuQVZZQUJRSEZTbVlNVGtDaEIzeTlpcHBrUWdrQUFBQUFBQUFBd0FBQUFBQUFBQUNmQUN0V0FBLiIsInNjcCI6IkFwcC5SZWFkLkFsbCBDYXBhY2l0eS5SZWFkLkFsbCBDYXBhY2l0eS5SZWFkV3JpdGUuQWxsIENvbm5lY3Rpb24uUmVhZC5BbGwgQ29ubmVjdGlvbi5SZWFkV3JpdGUuQWxsIENvbnRlbnQuQ3JlYXRlIERhc2hib2FyZC5SZWFkLkFsbCBEYXNoYm9hcmQuUmVhZFdyaXRlLkFsbCBEYXRhZmxvdy5SZWFkLkFsbCBEYXRhZmxvdy5SZWFkV3JpdGUuQWxsIERhdGFzZXQuUmVhZC5BbGwgRGF0YXNldC5SZWFkV3JpdGUuQWxsIEdhdGV3YXkuUmVhZC5BbGwgR2F0ZXdheS5SZWFkV3JpdGUuQWxsIEl0ZW0uRXhlY3V0ZS5BbGwgSXRlbS5FeHRlcm5hbERhdGFTaGFyZS5BbGwgSXRlbS5SZWFkV3JpdGUuQWxsIEl0ZW0uUmVzaGFyZS5BbGwgT25lTGFrZS5SZWFkLkFsbCBPbmVMYWtlLlJlYWRXcml0ZS5BbGwgUGlwZWxpbmUuRGVwbG95IFBpcGVsaW5lLlJlYWQuQWxsIFBpcGVsaW5lLlJlYWRXcml0ZS5BbGwgUmVwb3J0LlJlYWRXcml0ZS5BbGwgUmVwcnQuUmVhZC5BbGwgU3RvcmFnZUFjY291bnQuUmVhZC5BbGwgU3RvcmFnZUFjY291bnQuUmVhZFdyaXRlLkFsbCBUZW5hbnQuUmVhZC5BbGwgVGVuYW50LlJlYWRXcml0ZS5BbGwgVXNlclN0YXRlLlJlYWRXcml0ZS5BbGwgV29ya3NwYWNlLkdpdENvbW1pdC5BbGwgV29ya3NwYWNlLkdpdFVwZGF0ZS5BbGwgV29ya3NwYWNlLlJlYWQuQWxsIFdvcmtzcGFjZS5SZWFkV3JpdGUuQWxsIiwic2lkIjoiMDAzMjNiNzktNWQ0Yi01ZGUzLTI4MDMtNjUzZTE0MmQ5ZDk4Iiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoidDBQNlRYSHJfNWp4RV8zTGpQN3kyRVhhRlkyNy1ocjJ6TUU5Q3lIb0Y0ayIsInRpZCI6IjRhYzUwMTA1LTBjNjYtNDA0ZS1hMTA3LTdjYmQ4YTlhNjQ0MiIsInVuaXF1ZV9uYW1lIjoiYW5raXQuZEBTaWdtb2lkYW5hbHl0aWNzLmNvbSIsInVwbiI6ImFua2l0LmRAU2lnbW9pZGFuYWx5dGljcy5jb20iLCJ1dGkiOiJBNjNqbTlqanowR3JXN0RDYk53a0FBIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX2lkcmVsIjoiMSAzMCJ9.fFdYK-QceHaks2Cob-G0hTaFMnxnfKIl-WZub16fQ64h4ehX3hpJFyNk--ti583k42iVza-HyXgdzjoQAfIadOtZ7Fo7TeMcgK07ZcTXs83fD1sek-KGljpOl-Zn1B_sBW0pEYF3sVwidWTGtkbDgrNyB6c4F1nSQloP-IYdD4sLm3hE8j_GaXFQQrJxcA-AMANOae7IP_WnUxq_rWoLubzNSRs7UTyakXiTJozmzNbwxtLnKNEBS2eakd4KG4fbkeDxccxPl77E5z7R05c6aDw2hrD0RSJR_iwr7hp3aJ70GHkQtWOreBPXZ4UzV6v67T5EAtoXg8CCf1UEYdDQwA"
+
+remove_nulls = False
+remove_duplicates = False
+selected_table = None
+output_table = None
+
+
+
+def json_to_base64(json_Def):
+    json_string = json.dumps(json_Def)
+    base64_encoded_string = base64.b64encode(json_string.encode('utf-8')).decode('utf-8')
+
+    return base64_encoded_string
 
 def call_adf_api(endpoint, payload=None):
     headers = {
@@ -166,13 +186,252 @@ elif st.session_state["page"] == "Load Data":
     st.title("Load Data")
     data_destination = st.selectbox("Choose Data Destination", ["Onelake"], key="destination")
     output_table = st.text_input("Output Table Name", key="output_table")
+
     if st.button("Confirm"):
         st.session_state["data_destination"] = data_destination
-        st.session_state["output_table"] = output_table
+
+        filter_transformations = [
+            t for t in st.session_state.get("transformations", []) if t.get("type") == "Filter"
+        ]
+
+        remove_column_transformations = [
+            t for t in st.session_state.get("transformations", []) if t.get("type") == "Remove Column"
+        ]
+
+        parameters = {}
+
+        if remove_column_transformations:
+            parameters["remove_column"] = {
+                "value": remove_column_transformations[0]["columns"][0],
+                "type": "string"
+            }
+
+        if filter_transformations:
+            parameters["filter_column_value"] = {
+                "value": f"{filter_transformations[0]['column']}:{filter_transformations[0]['value']}",
+                "type": "string"
+            }
+
+        pipeline_definition = {
+            "properties": {
+                "activities": [
+                    {
+                        "name": "Extract",
+                        "type": "Copy",
+                        "dependsOn": [],
+                        "policy": {
+                            "timeout": "0.12:00:00",
+                            "retry": 0,
+                            "retryIntervalInSeconds": 30,
+                            "secureOutput": "false",
+                            "secureInput": "false"
+                        },
+                        "typeProperties": {
+                            "source": {
+                                "type": "LakehouseTableSource",
+                                "datasetSettings": {
+                                    "annotations": [],
+                                    "linkedService": {
+                                        "name": "strawlake",
+                                        "properties": {
+                                            "annotations": [],
+                                            "type": "Lakehouse",
+                                            "typeProperties": {
+                                                "workspaceId": "28b51e78-d498-45f6-b9e8-6e22cc442754",
+                                                "artifactId": "c347f46e-9c07-4a77-b726-2bbf4639511e",
+                                                "rootFolder": "Tables"
+                                            }
+                                        }
+                                    },
+                                    "type": "LakehouseTable",
+                                    "schema": [],
+                                    "typeProperties": {
+                                        "table": st.session_state["selected_table"]
+                                    }
+                                }
+                            },
+                            "sink": {
+                                "type": "LakehouseTableSink",
+                                "tableActionOption": "OverwriteSchema",
+                                "partitionOption": "None",
+                                "datasetSettings": {
+                                    "annotations": [],
+                                    "linkedService": {
+                                        "name": "strawlake_inter",
+                                        "properties": {
+                                            "annotations": [],
+                                            "type": "Lakehouse",
+                                            "typeProperties": {
+                                                "workspaceId": "28b51e78-d498-45f6-b9e8-6e22cc442754",
+                                                "artifactId": "d7689f98-b30a-45b9-b58f-b6b9f7fd62fb",
+                                                "rootFolder": "Tables"
+                                            }
+                                        }
+                                    },
+                                    "type": "LakehouseTable",
+                                    "schema": [],
+                                    "typeProperties": {
+                                        "table": "Extracted_Data"
+                                    }
+                                }
+                            },
+                            "enableStaging": "false",
+                            "translator": {
+                                "type": "TabularTranslator",
+                                "typeConversion": "true",
+                                "typeConversionSettings": {
+                                    "allowDataTruncation": "true",
+                                    "treatBooleanAsNumber": "false"
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "name": "Cleaning",
+                        "type": "TridentNotebook",
+                        "dependsOn": [
+                            {
+                                "activity": "Extract",
+                                "dependencyConditions": [
+                                    "Succeeded"
+                                ]
+                            }
+                        ],
+                        "policy": {
+                            "timeout": "0.12:00:00",
+                            "retry": 0,
+                            "retryIntervalInSeconds": 30,
+                            "secureOutput": "false",
+                            "secureInput": "false"
+                        },
+                        "typeProperties": {
+                            "notebookId": "abeab06f-1b94-4f3e-a548-5c03f0837fd1",
+                            "workspaceId": "28b51e78-d498-45f6-b9e8-6e22cc442754",
+                            "parameters": {
+                                "remove_nulls": {
+                                    "value": "True" if st.session_state["remove_nulls"] else "False",
+                                    "type": "bool"
+                                },
+                                "remove_duplicates": {
+                                    "value": "True" if st.session_state["remove_duplicates"] else "False",
+                                    "type": "bool"
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "name": "Transformation",
+                        "type": "TridentNotebook",
+                        "dependsOn": [
+                            {
+                                "activity": "Cleaning",
+                                "dependencyConditions": [
+                                    "Succeeded"
+                                ]
+                            }
+                        ],
+                        "policy": {
+                            "timeout": "0.12:00:00",
+                            "retry": 0,
+                            "retryIntervalInSeconds": 30,
+                            "secureOutput": "false",
+                            "secureInput": "false"
+                        },
+                        "typeProperties": {
+                            "notebookId": "d831038e-8c67-4b11-ba39-ea50cfad8a22",
+                            "workspaceId": "28b51e78-d498-45f6-b9e8-6e22cc442754",
+                            "parameters": parameters
+                        }
+                    },
+                    {
+                        "name": "Load",
+                        "type": "Copy",
+                        "dependsOn": [
+                            {
+                                "activity": "Transformation",
+                                "dependencyConditions": [
+                                    "Succeeded"
+                                ]
+                            }
+                        ],
+                        "policy": {
+                            "timeout": "0.12:00:00",
+                            "retry": 0,
+                            "retryIntervalInSeconds": 30,
+                            "secureOutput": "false",
+                            "secureInput": "false"
+                        },
+                        "typeProperties": {
+                            "source": {
+                                "type": "LakehouseTableSource",
+                                "datasetSettings": {
+                                    "annotations": [],
+                                    "linkedService": {
+                                        "name": "strawlake_inter",
+                                        "properties": {
+                                            "annotations": [],
+                                            "type": "Lakehouse",
+                                            "typeProperties": {
+                                                "workspaceId": "28b51e78-d498-45f6-b9e8-6e22cc442754",
+                                                "artifactId": "d7689f98-b30a-45b9-b58f-b6b9f7fd62fb",
+                                                "rootFolder": "Tables"
+                                            }
+                                        }
+                                    },
+                                    "type": "LakehouseTable",
+                                    "schema": [],
+                                    "typeProperties": {
+                                        "table": "Transformed_Data"
+                                    }
+                                }
+                            },
+                            "sink": {
+                                "type": "LakehouseTableSink",
+                                "tableActionOption": "Append",
+                                "partitionOption": "None",
+                                "datasetSettings": {
+                                    "annotations": [],
+                                    "linkedService": {
+                                        "name": "strawlake",
+                                        "properties": {
+                                            "annotations": [],
+                                            "type": "Lakehouse",
+                                            "typeProperties": {
+                                                "workspaceId": "28b51e78-d498-45f6-b9e8-6e22cc442754",
+                                                "artifactId": "c347f46e-9c07-4a77-b726-2bbf4639511e",
+                                                "rootFolder": "Tables"
+                                            }
+                                        }
+                                    },
+                                    "type": "LakehouseTable",
+                                    "schema": [],
+                                    "typeProperties": {
+                                        "table": st.session_state["output_table"]
+                                    }
+                                }
+                            },
+                            "enableStaging": "false",
+                            "translator": {
+                                "type": "TabularTranslator",
+                                "typeConversion": "true",
+                                "typeConversionSettings": {
+                                    "allowDataTruncation": "true",
+                                    "treatBooleanAsNumber": "false"
+                                }
+                            }
+                        }
+                    }
+                ],
+                "lastModifiedByObjectId": "8cec6a79-5f08-490b-9433-857a104ee019",
+                "lastPublishTime": "2025-03-29T20:08:19Z"
+            }
+        }
+
+        
         with st.spinner("Creating Data Pipeline..."):
             time.sleep(2)
-        st.session_state["page"] = "Pipeline Success"
-        st.rerun()
+            st.session_state["page"] = "Pipeline Success"
+            st.rerun()
 
 ######################
 # Page 5: Pipeline Success
